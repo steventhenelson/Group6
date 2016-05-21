aa0de942b76ed5572747f56f7bad84fcf211e29c
Tyler Burdsall <tylerburdsall@gmail.com>
2016-05-21 12:43:05 -0700

    Added framework for UserSession. Also, a 'fancier' login screen should be implemented later

--- ChocAn/ChocAn/UiLogic/UserSession.cs
+++ ChocAn/ChocAn/UiLogic/UserSession.cs
@@ -1,10 +1,38 @@
-﻿using ChocAn.Models;
-using ChocAn.Repositories;
+using ConsoleUi.Models;
+using ConsoleUi.Repositories;
 
-namespace ChocAn.UiLogic
+namespace ConsoleUi.UiLogic
 {
     public class UserSession
     {
+        private UserProfile _currentUser;
+        public UserProfile CurrentUser => _currentUser;
 
+        public void Login() {
+            UiTools tools = new UiTools();
+
+            // <--- A fancy console UX should be displayed here
+
+            // The user will enter a User ID number to login. The database will fill _currentUser if a valid ID is entered with the 
+            // correct information as necessary. The necessary menu will be displayed depending on the Role ID.
+            _currentUser = new UnitOfWork().UserProfileRepository.GetEntityById(tools.GetUserIdFromUser("Please enter in your User ID to login: "));
+            if (_currentUser == null) return;
+
+            switch (_currentUser.RoleId) {
+                case 2:
+                    ProviderUi providerUi = new ProviderUi(this);
+                    providerUi.Start();
+                    break;
+                case 3:
+                    OperatorUi operatorUi = new OperatorUi(this);
+                    operatorUi.Start();
+                    break;
+                case 4:
+                    ManagerUi managerUi = new ManagerUi(this);
+                    managerUi.Start();
+                    break;
+            }
+        }
     }
 }
+

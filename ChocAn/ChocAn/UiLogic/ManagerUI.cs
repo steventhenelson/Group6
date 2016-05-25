991c54f30231a5a6d7092331992489b5711e071b
steventhenelson <stnelson@pdx.edu>
2016-05-24 17:41:44 -0700

    Update ManagerUI.cs
    Populated the file with the Manager UI code.

--- ChocAn/ChocAn/UiLogic/ManagerUI.cs
+++ ChocAn/ChocAn/UiLogic/ManagerUI.cs
@@ -4,6 +4,37 @@ namespace ChocAn.UiLogic
 {
     public class ManagerUi : UiTools
     {
+        private UserSession _session;
 
+        public ManagerUi(UserSession session)
+        {
+            if (session.CurrentUser.RoleId != 4) throw new InvalidRoleException();
+            _session = session;
+        }
+
+        // controls the managers main menu accepts and returns nothing whatsoever
+        public void Start()
+        {
+            bool repeat = true;
+
+            while (repeat)
+            {
+                Console.Clear();
+                Console.WriteLine("Manager UI");
+                Console.WriteLine("1. Print summary report");
+                Console.WriteLine("2. Logout");
+
+                switch (GetIntegerFromUser(1, 2))
+                {
+                    case 1:
+                        Reports report = new Reports();
+                        report.PrintSummaryReport(GetFilePathFromUser());
+                        break;
+                    case 2:
+                        repeat = false;
+                        break;
+                }
+            }
+        }
     }
 }
